e9e6394615b554ac109a02ff2b67401211cca6fc
kikiindiani <127711150+kikiindiani@users.noreply.github.com>
2023-06-01 14:24:58 +0700

    Add files via upload

---
+++ praktikum 3/overload1.py
@@ -0,0 +1,6 @@
+# Nama  : Kiki Indiani
+# Kelas : R1
+# NIM   : 210511046
+
+print(len("geeks"))
+print(len([10, 20, 30]))
---
+++ praktikum 3/overload2.py
@@ -0,0 +1,7 @@
+# Nama  : Kiki Indiani
+# Kelas : R1
+# NIM   : 210511046
+
+print(5 - 1)
+print("Above" + " The" + " Clouds")
+print([1, 2] + [3, 4]) 
